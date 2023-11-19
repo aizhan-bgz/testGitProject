a137622081fc95b527d3754c5486f01a2066b886
aizhan-bgz <baigazakova1999@gmail.com>
2023-11-19 17:50:49 +0600

    first commit

---
+++ src/Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+    private String name;
+}
+/*
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/aizhan-bgz/testGitProject.git
+  git push -u origin main
+
+
+
+
+
+ */
