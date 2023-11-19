1eb4f92f9c9b662193207daccf9182d57ef76abd
aizhan-bgz <baigazakova1999@gmail.com>
2023-11-19 18:14:02 +0600

    добавил конструкторы и методы доступа

--- src/Animal.java
+++ src/Animal.java
@@ -1,16 +1,15 @@
 public class Animal {
     private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
-/*
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/aizhan-bgz/testGitProject.git
-  git push -u origin main
-
-
-
-
-
- */
